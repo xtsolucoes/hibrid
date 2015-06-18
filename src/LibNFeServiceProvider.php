4da6eb68238ad19da68ac08bf559e44e95826fb2
Roberson Faria <roberson.faria@gmail.com>
2015-06-17 23:07:36 -0300

    Creado service provider

---
+++ src/LibNFeServiceProvider.php
@@ -0,0 +1,19 @@
+<?php namespace RfWeb\LibNFe;
+
+use Illuminate\Support\ServiceProvider;
+
+class LibNFeServiceProvider extends ServiceProvider {
+
+
+	public function boot(){
+		$this->publishes([
+				__DIR__.'/config/LibNFe.php' => config_path('LibNFe.php'),
+		]);
+	}
+	
+    public function register()
+    {
+        //
+    }
+
+}
